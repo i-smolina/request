e277a4e46d632f8a4a03d7432e5c8abe33cce109
i-smolina <smolirinka@gmail.com>
2021-12-08 22:55:08 +0300

    Use mysql

---
+++ request/src/test/java/ru/smolina/request/RequestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smolina.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
